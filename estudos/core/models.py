16bf773a33736ba37875d65bff9d414bdd1eeb4f
LuskGPM <145815917+LuskGPM@users.noreply.github.com>
2025-09-26 10:49:17 -0300

    V1.0.4

--- estudos/core/models.py
+++ estudos/core/models.py
@@ -5,3 +5,11 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
+    cpf = models.CharField(max_length=11, unique=True, default='')
+    ativo = models.BooleanField(default=True)
+    data_nascimento = models.DateField(null=True, blank=True)
+    
+class Fatura(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    pago = models.BooleanField(default=False)
