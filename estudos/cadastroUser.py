d0a2efee6324f9302b2d46e1cf1bf1f95825740c
LuskGPM <145815917+LuskGPM@users.noreply.github.com>
2025-09-26 10:54:28 -0300

    cadastro melhorado V1.0.5

--- estudos/cadastroUser.py
+++ estudos/cadastroUser.py
@@ -4,12 +4,30 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "estudos.settings")
 django.setup()
 
-from core.models import User
+from core.models import User, Fatura
 
 def createUser():
-    usuario = User(name = "Lucas", email = "teste@teste.com")
-    usuario.save()
+    from datetime import datetime
+    data_nasc = datetime.date(2005, 6, 13)
     
+    user = User(
+        name='Lucas Melo', 
+        email='teste2@teste.com', 
+        cpf='12345678911', 
+        data_nascimento=data_nasc
+        )
+    
+    fatura = Fatura(
+        user=user,
+        valor=100.00,
+        pago=False
+    )
+    
+    user.save()
+    fatura.save()
+    
+    
+
 if __name__ == "__main__":
     createUser()
     
